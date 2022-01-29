81e7b3ffa64806b87b2115f16e1d5592b495a1fa
nemishappy <58110088+nemishappy@users.noreply.github.com>
2022-01-30 00:50:32 +0700

    add tree pose, triangle pose, warrior

--- lib/detect_pose/detection_warrior_pose_widget.dart
+++ lib/detect_pose/detection_warrior_pose_widget.dart
@@ -1,5 +1,4 @@
 import 'package:yogaer/flutter_flow/flutter_flow_theme.dart';
-import 'package:yogaer/services/render_data_arm_press.dart';
 
 import '../flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:tflite/tflite.dart';
 
-import 'package:yogaer/services/camera.dart';
+import 'package:yogaer/camera.dart';
+import 'package:yogaer/bndbox.dart';
 
 class DetectionWarriorPoseWidget extends StatefulWidget {
   final List<CameraDescription> cameras;
@@ -22,7 +22,7 @@ class DetectionWarriorPoseWidget extends StatefulWidget {
 class _DetectionWarriorPoseWidgetState
     extends State<DetectionWarriorPoseWidget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
-  List<dynamic> _data;
+  List<dynamic> _recognitions;
   int _imageHeight = 0;
   int _imageWidth = 0;
   int x = 1;
@@ -39,7 +39,7 @@ class _DetectionWarriorPoseWidgetState
       return;
     }
     setState(() {
-      _data = data;
+      _recognitions = data;
       _imageHeight = imageHeight;
       _imageWidth = imageWidth;
     });
@@ -62,16 +62,17 @@ class _DetectionWarriorPoseWidgetState
       ),
       body: Stack(
         children: <Widget>[
-          CameraApp(
+          Camera(
             cameras: widget.cameras,
             setRecognitions: _setRecognitions,
           ),
-          RenderDataArmPress(
-            data: _data == null ? [] : _data,
+          BndBox(
+            results: _recognitions == null ? [] : _recognitions,
             previewH: max(_imageHeight, _imageWidth),
             previewW: min(_imageHeight, _imageWidth),
             screenH: screen.height,
             screenW: screen.width,
+            customModel: "Virbhadrasana",
           ),
         ],
       ),
---
+++ lib/util/pose_data.dart
@@ -0,0 +1,22 @@
+List<String> beginnerAsanas = [
+  'Trikonasana', // teiangle pose modeled
+  'Vrikshasana', // tree pose modeled
+  'Virbhadrasana', // warrior pose modeled
+  'Bhujangasana', // Cobra Pose
+  'Sukhasana', // seated pose
+  'Tadasana'
+];
+
+List<String> intermediateAsanas = [
+  'Dhanurasana',
+  'Padhastasana',
+  'Ustrasana',
+  'Halasana',
+  'Sarvangasana' // bridg
+];
+
+List<String> advanceAsanas = [
+  'Bakasana',
+  'Mayurasana',
+  'Sirsasana',
+];
